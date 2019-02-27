b927f51de1ddd0381664124a4d9927ef979c89f8
Jerod Rubalcava <jerodruby@hotmail.com>
2019-02-26 17:46:13 -0800

    More JS

--- js/app.js
+++ js/app.js
@@ -79,29 +79,32 @@ var data = {
   current:0,
   open_idx:0
 };
+var point1 = {};
+var point2 = {};
 
 var canvas_click = function(event){
+  //console.log(point1.x, bg, color);
   var mouse = {
     x:event.layerX,
     y:event.layerY
   };  
-  var point1 = {};
-  var point2 = {};
   var pixel = ctx.getImageData(mouse.x, mouse.y, 1, 1).data;
-  var color = `rgb(${pixel[0]}, ${pixel[1]}, ${pixel[2]})`;
+  var color = `rgb(${pixel[0]},${pixel[1]},${pixel[2]})`;
   var bg = data.images[data.current].bg_color;
   var fg = data.images[data.current].fg_color;
-  console.log(color, bg, fg);
-  // if(color === bg){ //if color = red then we need it to hold onto the first point, 
+  //console.log(color, bg, fg);
+  if(color !== bg && !point1.x){ //if color = red then we need it to hold onto the first point, 
   //   //if first click is valid, start saving data points, if not don't save
-  //   console.log('hit');
-  //   point1.x = mouse.x;
-  //   point1.y = mouse.y;
-  //   if(color !== bg){
-  //     point2.x = mouse.x;
-  //     point2.y = mouse.y;
-  //   }
-  // }
+    console.log('hit');
+    point1.x = mouse.x;
+    point1.y = mouse.y;
+  } else point1 = {};
+    if(point1.x){
+      console.log('hit2', bg, color);
+      point2.x = mouse.x;
+      point2.y = mouse.y;
+    }
+    console.log(point1, point2);
   // ctx.beginPath();
   // ctx.fillStyle = fg;
   // ctx.arc(point1.x, point1.y, 10, 0, Math.PI*2);
