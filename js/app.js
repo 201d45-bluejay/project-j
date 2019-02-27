3e08bd766062facef0c807bb73c0000236d45a5e
Jesse Van Volkinburg <vanvoljg@gmail.com>
2019-02-27 13:44:33 -0800

    reset function complete, line drawing function complete

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ var Image_Data = function () {
   this.current = 0;
   this.open_idx = 0;
   this.images = [];
+  this.newImg = true;
   for (var i = 0; i < 12; i++) {
     this.images.push(new Img());
   }
@@ -39,22 +40,51 @@ var canvas_click = function(event){
     ctx.strokeStyle= 'green';
     ctx.lineWidth = 2;
     ctx.stroke();
-  } else if (color === bg && point1.x) {
+  } else if (point1.x) {
     console.log('bghit2');
     point2.x = mouse.x;
     point2.y = mouse.y;
     working.points.push({ x1: point1.x, y1: point1.y, x2: point2.x, y2: point2.y });
     point1 = {};
     point2 = {};
-    console.log(data);
+    console.log('data:', data);
+    draw();
   } else {
+    draw();
     point1 = {};
   }
   console.log(working);
 };
 
+var draw = function() {
+  clear();
+  // draw base image
+  ctx.beginPath();
+  ctx.fillStyle = working.fg_color;
+  ctx.arc(50, 50, 10, 0, Math.PI * 2);
+  ctx.fill();
+
+  // array of points, draws a single thick line per point set
+  for (var i = 0; i < working.points.length; i++) {
+    ctx.beginPath();
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'butt';
+    ctx.strokeStyle = working.fg_color;
+    ctx.moveTo(working.points[i].x1, working.points[i].y1);
+    ctx.lineTo(working.points[i].x2, working.points[i].y2);
+    ctx.stroke();
+  }
+};
+
+var clear = function() {
+  ctx.fillStyle = working.bg_color;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 var reset = function(){
+  working.points = [];
   console.log('reset');
+  draw();
 };
 
 var overwrite_check = function(){
@@ -80,12 +110,20 @@ var save = function(){
 var retrieve = function(){
   if (localStorage.getItem('nature_images')){
     data = JSON.parse(localStorage.getItem('nature_images'));
-    working = data.images[data.current];   
-  }
-  else {
+    // if we're getting data after picking an image from the gallery to edit,
+    // then we should load that one.
+    if (data.newImg) {
+      // otherwise, working should be new
+      working = new Img();
+      return;
+    }
+    working = data.images[data.current];
+    console.log(data, working);
+  } else {
     data = new Image_Data();
     // console.log(data);
     working = new Img();
+    console.log('empty', data, working);
   }
 };
 
@@ -97,7 +135,7 @@ var click_handler = function(event) {
   else if (event.target.id === 'reset_button') {
     reset();
   } else if (event.target.id === 'save_button') {
-    data.open_idx = 12;   //TODO:delete when done
+    // data.open_idx = 12;   //TODO:delete when done
     console.log(data);
     save();
     // console.log('save');
@@ -151,11 +189,13 @@ var ctx = canvas.getContext('2d');
 
 retrieve();
 
-ctx.fillStyle = data.images[data.current].bg_color;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.fillStyle = data.images[data.current].fg_color;
-ctx.arc(50, 50, 10, 0, Math.PI*2);
-ctx.fill();
+draw();
+
+// ctx.fillStyle = data.images[data.current].bg_color;
+// ctx.fillRect(0, 0, canvas.width, canvas.height);
+// ctx.fillStyle = data.images[data.current].fg_color;
+// ctx.arc(50, 50, 10, 0, Math.PI*2);
+// ctx.fill();
 
 
 //local storage data handling: loading data, saving data, (data format - single object). Object id and array of things that have been added to canvas (undo if necessary)
